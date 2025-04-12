12ca496b037af965a6919113f4f1ce0430db4dab
Gauri <kalmathgauri@gmail.com>
2025-04-12 13:31:18 +0530

    basic header changes

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.write("Hello world")
+st.markdown("""
+    <h1 style='
+        text-align: center;
+        background: linear-gradient(to right, red, orange, yellow, green);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-family: "Segoe UI", sans-serif;
+    '>
+        🎮Streamle - A Wordle Game
+    </h1>
+""", unsafe_allow_html=True)
+
+st.subheader("Guess the 5-letter word in 6 tries or less.")
